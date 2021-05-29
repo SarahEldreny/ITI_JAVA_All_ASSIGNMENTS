c1e38e672de3b023a7864fda41aca252ed7401e6
saraheldreny <sarahdreny11@gmail.com>
2021-05-30 01:43:33 +0200

    initial trial

---
+++ Day3/company/com/checkerString.java
@@ -0,0 +1,9 @@
+package company.com;
+
+import java.util.function.Predicate;
+
+public class checkerString {
+    public static boolean checkString(String string, Predicate<String> predicate){
+        return predicate.test(string);
+    }
+}
